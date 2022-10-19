2032b48839055435070235b2d31203847817c2ad
Daniel Bencz <daniel.bencz@gmail.com>
2022-10-19 21:38:12 +0200

    add routing

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,17 @@
 import styles from "../styles/Layout.module.css";
+import Nav from "./Nav";
 
 export const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Hello</h1>
-        {children}
-      </main>
-    </div>
+    <>
+      <Nav></Nav>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1>Hello</h1>
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
